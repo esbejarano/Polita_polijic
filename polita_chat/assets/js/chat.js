4be558a8bda19bc248a5afef8d97132bfedd199c
esbejarano <erick.935@gmail.com>
2018-09-09 23:00:22 -0500

    Tercer commit
    Se agrega nuevo view para el chat

---
+++ polita_chat/assets/js/chat.js
@@ -0,0 +1,76 @@
+//var ws = new WebSocket("ws://localhost:8000");
+//$(window).on('beforeunload', function(){ ws.close(); });
+//ws.onerror = function(event) { location.reload();}
+/*
+ws.onmessage = function(event)  { 
+	var message_received = event.data;
+	chat_add_message(message_received, false);
+};
+*/
+
+
+
+/*
+$(function() {
+	$('#chat_input').on('keypress', function(event) {
+		if (event.which === 13 && $(this).val() != ""){
+			var message = $(this).val();
+			$(this).val("");
+			chat_add_message(message, true);
+			//ws.send(message);
+		}
+	});
+});
+*/
+	
+
+function chat_add_message(message, isUser) {
+	var class_prefix = 'chatbox__body__message chatbox__body__message';
+	var icon_person = '<span class="oi oi-person"></span>';
+	var class_suffix = isUser ? '--right' : '--left';
+	var html = '<div class="' + class_prefix + class_suffix + '">' + icon_person + '<p>' + message + '</p>';
+	chat_add_html(html);
+}
+	
+function chat_add_html(html) {
+	$(".chatbox__body").append(html);
+	chat_scrolldown();
+}
+
+function chat_scrolldown() {
+	$(".chatbox__body").animate({ 
+		scrollTop: $(".chatbox__body")[0].scrollHeight 
+	}, 500);
+}
+	
+$(document).ready(function() {
+		
+	$('.chatbox__title').on('click', function() {
+		$('.chatbox').toggleClass('chatbox--tray');
+	});
+
+	$('.chatbox__title__close').on('click', function(e) {
+		e.stopPropagation();
+		$('.chatbox').addClass('chatbox--closed');
+	});
+	
+	$('.chatbox').on('transitionend', function() {
+		if ($('.chatbox').hasClass('chatbox--closed')) {
+			$('.chatbox').remove();
+		}
+	});
+	
+	$('.chatbox__message').on('keypress', function(event) {
+		if (event.which === 13 && $(this).val() != ""){
+			var message = $(this).val();
+			$(this).val("");
+			chat_add_message(message, true);
+			//ws.send(message);
+		}
+	});
+	
+	$('.chatbox__credentials').on('submit', function(e) {
+		e.preventDefault();
+		$('.chatbox').removeClass('chatbox--empty');
+	});
+});
